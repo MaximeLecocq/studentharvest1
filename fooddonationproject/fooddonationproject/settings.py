6b89794837461165cfa86bf2228f29429bf88c00
Maxime <mmlecocq@gmail.com>
2024-09-02 07:26:15 +0200

    Added IP to ALLOWED_HOSTS

--- fooddonationproject/fooddonationproject/settings.py
+++ fooddonationproject/fooddonationproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w^_+rvrr6f#j+eg(ehkfc6x^gh_mpbz9zhsref0@xs=(433#xf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.48.44.205']
 
 
 # Application definition
